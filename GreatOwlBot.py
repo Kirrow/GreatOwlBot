e8b86dd23158e3abd24ba84559f5f4b6b2a0aadc
z00n <Justin.Podzunas@ibm.com>
2019-02-02 21:37:26 -0600

    Added role management functionality.  Made some changes to help text.

--- GreatOwlBot.py
+++ GreatOwlBot.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env
+
 import re
 import discord
 import requests
 
-TOKEN = 'TOKEN GOES HERE'
+TOKEN = ''
 
 client = discord.Client()
 
@@ -19,7 +20,7 @@ async def on_message(message):
     if message.content.startswith('go!help'):
         em = discord.Embed(
             title='Great Owl Help',
-            description='go!help: Displays this help message\ngo!coriolis: Links Coriolis website\ngo!eddb: Links Elite Dangerous Database website\ngo!edrefcard: Links EDRefCard website\ngo!edsm: Links Elite Dangerous Star Map website\ngo!inara: Links INARA website',
+            description='Help Commands:\ngo!help: Displays this help message\ngo!rolehelp: Displays role command help\n\nWebsite Link Commands:\ngo!coriolis: Coriolis website\ngo!eddb: Elite Dangerous Database website\ngo!edrefcard: EDRefCard website\ngo!edsm: Elite Dangerous Star Map website\ngo!inara: INARA website',
             color=0x00fcff
             )
         em.set_image(
@@ -34,6 +35,9 @@ async def on_message(message):
             url='https://coriolis.io',
             description='A ship builder, outfitting and comparison tool for Elite Dangerous',
             color=0x00fcff
+            )
+        em.add_field(
+            name='Note', value='***Please use the s.orbis built-in short link when sharing builds! [Link icon on top right of coriolis page]***'
             )
         em.set_image(
             url='https://coriolis.io/mstile-144x144.png'
@@ -104,6 +108,75 @@ async def on_message(message):
             url='https://www.edsm.net/img/favicons/mstile-144x144.png'
             ) 
         await client.send_message(message.channel, embed=em)
+
+    #Role help response
+    if message.content.startswith('go!rolehelp'):
+        em = discord.Embed(
+            title='Great Owl Role Help',
+            description='go!pc: Add yourself to the PC role\ngo!xb1: Add yourself to the Xbox 1 role\ngo!ps4: Add yourself to the PS4 role\n\nIf you run a command while already assigned to its role, you will be asked if you wish to be removed from the role.',
+            color=0x00fcff
+            )
+        em.set_image(
+            url='https://i.imgur.com/7yG4taS.jpg'
+            )
+        await client.send_message(message.channel, embed=em)
+
+    #Role Assignment PC
+    if message.content.startswith('go!pc'):
+        role = discord.utils.get(message.server.roles, name='PC')
+        if discord.utils.get(message.author.roles, name='PC'):
+            msg = 'You already have the PC role. Would you like to be removed from that role, CMDR?\n(y/n)'
+            await client.send_message(message.channel, msg)
+            reply = await client.wait_for_message(timeout=30, author=message.author)
+            if reply == None:
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+            elif reply.content == 'y':
+                await client.remove_roles(message.author, role)
+                await client.send_message(message.channel, 'You have been removed from the PC role.')
+            elif reply.content == 'n':
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+        else:
+            msg = 'Adding you to the PC role, CMDR.'
+            await client.add_roles(message.author, role)
+            await client.send_message(message.channel, msg)
+
+    #Role Assignment XB1
+    if message.content.startswith('go!xb1'):
+        role = discord.utils.get(message.server.roles, name='XBOX 1')
+        if discord.utils.get(message.author.roles, name='XBOX 1'):
+            msg = 'You already have the Xbox 1 role. Would you like to be removed from that role, CMDR?\n(y/n)'
+            await client.send_message(message.channel, msg)
+            reply = await client.wait_for_message(timeout=30, author=message.author)
+            if reply == None:
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+            elif reply.content == 'y':
+                await client.remove_roles(message.author, role)
+                await client.send_message(message.channel, 'You have been removed from the Xbox 1 role.')
+            elif reply.content == 'n':
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+        else:
+            msg = 'Adding you to the Xbox 1 role, CMDR.'
+            await client.add_roles(message.author, role)
+            await client.send_message(message.channel, msg)
+
+    #Role Assignment PS4
+    if message.content.startswith('go!ps4'):
+        role = discord.utils.get(message.server.roles, name='PS4')
+        if discord.utils.get(message.author.roles, name='PS4'):
+            msg = 'You already have the PS4 role. Would you like to be removed from that role, CMDR?\n(y/n)'
+            await client.send_message(message.channel, msg)
+            reply = await client.wait_for_message(timeout=30, author=message.author)
+            if reply == None:
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+            elif reply.content == 'y':
+                await client.remove_roles(message.author, role)
+                await client.send_message(message.channel, 'You have been removed from the PS4 role.')
+            elif reply.content == 'n':
+                await client.send_message(message.channel, 'Your roles have not been modified, CMDR')
+        else:
+            msg = 'Adding you to the PS4 role, CMDR.'
+            await client.add_roles(message.author, role)
+            await client.send_message(message.channel, msg)
 
     #Coriolis autoshortener
     if all(chunk in message.content for chunk in ('coriolis.io/outfit/', '?code=')):
